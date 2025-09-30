48adbd88a047401af959840a111afcf29cc82832
admin <admin@localhost.localdomain>
2025-09-30 20:23:15 +0700

    Исправлен ввод ФИО

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char** argv) {
  char* str = (char*)malloc(sizeof(char) * SIZE);
  printf("Введите своё имя:\n");
- scanf("%s", str);
- printf("Привет, %s", str);
+ str = gets(str);
+puts("Привет");
+puts(str);
  return 0;
 }
